e93b20ebe6ed6c60799bbab093f67e83cb62f014
Ranjan1501 <ranjankumar1757@gmail.com>
2024-05-21 00:33:55 +0530

    add userSchema

---
+++ server/src/model/user.model.js
@@ -0,0 +1,60 @@
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    mobile: {
+      type: String,
+      required: true,
+      minlength: [10, "Mobile number should be of 10 digits"],
+      maxlength: [10, "Mobile number should be of 10 digits"],
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    profile_photo: {
+      type: String,
+      required: false,
+    },
+    about: {
+      type: String,
+      required: false,
+      default: "",
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  }
+);
+
+userSchema.pre("save", function (next) {
+  if (!this.isModified("password")) return next();
+  bcrypt.hash(this.password, 10, (err, hash) => {
+    this.password = hash;
+    return next();
+  });
+});
+
+userSchema.methods.checkPassword = function (password) {
+  return new Promise((resolve, reject) => {
+    bcrypt.compare(password, this.password, function (err, same) {
+      if (err) return reject(err);
+
+      return resolve(same);
+    });
+  });
+};
+
+const User = mongoose.model("users", userSchema);
+export default User;
