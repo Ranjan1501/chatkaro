3b1d2f9917100e7fa311316a51ff65dadc77599c
Ranjan1501 <ranjankumar1757@gmail.com>
2024-05-25 18:51:14 +0530

    set up App

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import MainContainer from "./Components/MainContainer";
+import { RoutePages } from "./Components/Route";
+
+function App() {
+  return (
+    <div className="App">
+      <RoutePages />
+      {/* <MainContainer /> */}
+    </div>
+  );
+}
+
+export default App;
