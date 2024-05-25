340cbd28420e03dde5f1b41a12c2fef4c62ce70b
Ranjan1501 <ranjankumar1757@gmail.com>
2024-05-25 18:34:36 +0530

    Add🚏:Route method

--- server/index.js
+++ server/index.js
@@ -1,9 +1,47 @@
 import express from "express";
-import { register } from "./src/controllers/user.controllers.js";
-import { login } from "./src/controllers/user.controllers.js";
+import cors from "cors";
+
+import {
+  registerUser,
+  loginUser,
+  userById,
+  getAllUsers,
+} from "./src/controllers/user.controllers.js";
+import {
+  getUserMessages,
+  getRoomMessages,
+  createMessage,
+} from "./src/controllers/message.controllers.js";
+import {
+  indexRoom,
+  deleteRoom,
+  updateRoom,
+  createRoom,
+} from "./src/controllers/rooms.controllers.js";
 const app = express();
+
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+
 app.use(express.json());
-app.post("/api/v1/auth/register/", register);
-app.post("api/v1/auth/login", login);
+
+app.post("/api/users/signup", registerUser);
+app.post("/api/users/login", loginUser);
+app.get("/api/users/:userId", userById);
+app.get("/api/users", getAllUsers);
+
+app.get("/api/messages/user/:userId", getUserMessages);
+app.get("/api/messages/group/:groupId", getRoomMessages);
+app.post("/api/messages", createMessage);
+
+app.get("/api/groups", indexRoom);
+app.post("/api/groups", createRoom);
+app.delete("/api/groups/:groupId", deleteRoom);
+app.put("/api/groups/:groupId", updateRoom);
 
 export default app;
