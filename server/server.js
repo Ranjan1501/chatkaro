8db4cba871c96508075ce5beb949357b77a8c716
Ranjan1501 <ranjankumar1757@gmail.com>
2024-05-27 01:15:55 +0530

    Add⚡️:File upload and socket execution

--- server/server.js
+++ server/server.js
@@ -1,12 +1,44 @@
+// import { connect } from "./src/config/db.js";
+// import app from "./index.js";
+// import dotenv from "dotenv";
+// dotenv.config();
+// const port = process.env.PORT;
+// app.listen(port, async () => {
+//   try {
+//     await connect();
+//     console.log(`listening on Port  ${port}`);
+//   } catch (err) {
+//     throw new Error("Error in establishing connection");
+//   }
+// });
+
+// import { connect } from "./src/config/db.js";
+// import { server } from "./index.js"; // Import the server instance
+// import dotenv from "dotenv";
+// dotenv.config();
+
+// const port = process.env.PORT || 4000;
+
+// server.listen(port, async () => {
+//   try {
+//     await connect();
+//     console.log(`Listening on port ${port}`);
+//   } catch (err) {
+//     throw new Error("Error in establishing connection");
+//   }
+// });
+
 import { connect } from "./src/config/db.js";
-import app from "./index.js";
+import server from "./index.js";
 import dotenv from "dotenv";
 dotenv.config();
-const port = process.env.PORT;
-app.listen(port, async () => {
+
+const port = process.env.PORT || 4000;
+
+server.listen(port, async () => {
   try {
     await connect();
-    console.log(`listening on Port  ${port}`);
+    console.log(`listening on Port ${port}`);
   } catch (err) {
     throw new Error("Error in establishing connection");
   }
