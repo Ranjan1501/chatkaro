1d830dafee1a33fca0eeb39c841ec622a947131d
Ranjan1501 <ranjankumar1757@gmail.com>
2024-05-27 01:15:41 +0530

    Add⚡️:File upload and socket execution

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import cors from "cors";
-
+import fileUpload from "express-fileupload";
+import http from "http";
+import { Server } from "socket.io";
 import {
   registerUser,
   loginUser,
@@ -18,7 +20,15 @@ import {
   updateRoom,
   createRoom,
 } from "./src/controllers/rooms.controllers.js";
+
 const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
 
 app.use(
   cors({
@@ -29,6 +39,7 @@ app.use(
 );
 
 app.use(express.json());
+app.use(fileUpload());
 
 app.post("/api/users/signup", registerUser);
 app.post("/api/users/login", loginUser);
@@ -44,4 +55,24 @@ app.post("/api/groups", createRoom);
 app.delete("/api/groups/:groupId", deleteRoom);
 app.put("/api/groups/:groupId", updateRoom);
 
-export default app;
+io.on("connection", (socket) => {
+  console.log("A user connected");
+
+  socket.on("sendMessage", (message) => {
+    if (message.groupId) {
+      io.to(message.groupId).emit("message", message);
+    } else if (message.userId) {
+      socket.to(message.userId).emit("message", message);
+    }
+  });
+
+  socket.on("joinRoom", (room) => {
+    socket.join(room);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("A user disconnected");
+  });
+});
+
+export default server;
