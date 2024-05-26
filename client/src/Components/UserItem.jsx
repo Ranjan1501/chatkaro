b8bb88fc1d896cc62b892cbaba89b1bd8580eda1
Ranjan1501 <ranjankumar1757@gmail.com>
2024-05-27 01:03:37 +0530

    Decorate🎄 user name

--- client/src/Components/UserItem.jsx
+++ client/src/Components/UserItem.jsx
@@ -1,20 +1,22 @@
-// UserItem.js
-import React from "react";
+// import React from "react";
 import PersonIcon from "@mui/icons-material/Person";
 import { IconButton } from "@mui/material";
 
-function UserItem({ user, onSelectUser }) {
+import React from "react";
+import "./style.css";
+
+export default function UserItem({ user, onSelectUser }) {
   return (
     <div className="user-item" onClick={() => onSelectUser(user._id)}>
+      {/* <img src={user.profile_photo} alt={user.name} className="user-avatar" /> */}
       <IconButton className="icon-btn">
         <PersonIcon />
       </IconButton>
-      <div>
-        <h3> {user.name}</h3>
-        <p> {user.mobile}</p>
+      <div className="user-details">
+        <div className="user-name">
+          <h3> {user.name}</h3>
+        </div>
       </div>
     </div>
   );
 }
-
-export default UserItem;
