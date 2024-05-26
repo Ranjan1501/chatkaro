4d8d2050f3bbfd361a7b7d3ce085da0ed9665bc5
Ranjan1501 <ranjankumar1757@gmail.com>
2024-05-27 01:01:55 +0530

    Fix🛠 :One to one and message broadcasting

--- client/src/Components/ChatArea.jsx
+++ client/src/Components/ChatArea.jsx
@@ -1,22 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 import "./style.css";
+import SendIcon from "@mui/icons-material/Send";
+import { IconButton } from "@mui/material";
 
-export default function ChatArea({ messages }) {
+export default function ChatArea({ messages, onSendMessage }) {
+  const [userMessage, setUserMessage] = useState("");
+
+  const handleSendMessage = () => {
+    if (userMessage.trim() !== "") {
+      onSendMessage(userMessage);
+      setUserMessage("");
+    }
+  };
+
+  const handleKeyPress = (e) => {
+    if (e.key === "Enter") {
+      handleSendMessage();
+    }
+  };
+
   return (
     <div className="chat-area">
-      {messages.length === 0 ? (
-        <div className="no-messages">
-          Select a user or group to start chatting.
-        </div>
-      ) : (
-        <div className="messages">
-          {messages.map((message, index) => (
+      <div className="messages">
+        {messages.length > 0 ? (
+          messages.map((msg, index) => (
             <div key={index} className="message-box">
-              <p>{message.message}</p>
+              <p>{msg.message}</p>
             </div>
-          ))}
-        </div>
-      )}
+          ))
+        ) : (
+          <div className="no-messages">
+            {" "}
+            Select a user or group to start chatting.
+          </div>
+        )}
+      </div>
+      <div className="message-input-container">
+        <input
+          type="text"
+          className="message-input"
+          value={userMessage}
+          onChange={(e) => setUserMessage(e.target.value)}
+          onKeyPress={handleKeyPress}
+          placeholder="message..."
+        />
+        <IconButton className="message-send-btn">
+          <SendIcon id="send" onClick={handleSendMessage} />
+        </IconButton>
+      
+      </div>
     </div>
   );
 }
