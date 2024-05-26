cf198f8ba699cbbb3b26d4e1e19475e031e36fb1
Ranjan1501 <ranjankumar1757@gmail.com>
2024-05-27 01:02:44 +0530

    FDecorate group name

--- client/src/Components/GroupItem.jsx
+++ client/src/Components/GroupItem.jsx
@@ -1,19 +1,21 @@
-// GroupItem.js
-import React from "react";
+// import React from "react";
 import GroupAddIcon from "@mui/icons-material/GroupAdd";
 import { IconButton } from "@mui/material";
 
-function GroupItem({ group, onSelectGroup }) {
+import React from "react";
+import "./style.css";
+
+export default function GroupItem({ group, onSelectGroup }) {
   return (
     <div className="group-item" onClick={() => onSelectGroup(group._id)}>
       <IconButton className="icon-btn">
         <GroupAddIcon />
       </IconButton>
-      <div>
-        <h3> {group.name} </h3>
+      <div className="group-details">
+        <div className="group-name">
+          <h3> {group.name}</h3>
+        </div>
       </div>
     </div>
   );
 }
-
-export default GroupItem;
