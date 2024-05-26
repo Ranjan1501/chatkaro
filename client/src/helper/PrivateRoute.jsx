63cb1d203666b6ab83f9bd606944d206da377771
Ranjan1501 <ranjankumar1757@gmail.com>
2024-05-26 14:40:12 +0530

    Set up private route 🕵️

---
+++ client/src/helper/PrivateRoute.jsx
@@ -0,0 +1,10 @@
+import { useContext } from "react";
+import { Navigate } from "react-router-dom";
+import { AuthContext } from "../Context/AuthContext";
+export const PrivateRoute = ({ children }) => {
+  const { token } = useContext(AuthContext);
+  if (!token) {
+    return <Navigate to={"/signin"} />;
+  }
+  return children;
+};
